27da226be0455140da0ed1db7d72d9a11e0a37cd
Jay Lee <dreampoetlee@gmail.com>
2022-04-17 22:17:16 -0400

    Created Super + SuperType Serializer Class and File

---
+++ super_types/serializers.py
@@ -0,0 +1,18 @@
+from pyexpat import model
+from rest_framework import serializers
+from .models import SuperType
+
+class SuperTypeSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = SuperType
+    fields = ['type']
+    
+    
+    pass
+  
+  
+  
+  
+  
+  
+  pass
---
+++ supers/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Supers
+
+class SupersSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Supers
+    fields = ['name', 'alter_ego', 'primary_ability', 'secondary_ability', 'catchphrase','super_type']
+    
+    pass
+  
+  
+  
+  pass
