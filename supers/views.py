1822f380da0e1fe6b00f4f66a846b2b74118aea3
Jay Lee <dreampoetlee@gmail.com>
2022-04-18 16:53:49 -0400

    Updated Project URLS + Created First View Function + Created Supers & Super_types App Url Files

--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+import imp
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(['GET'])
+def supers_list(request):
+  
+  
+  
+    return Response('ok')
+  
