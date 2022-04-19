95ddb5175ddf38f5fc6f136ad1d25e7d1301d588
Jay Lee <dreampoetlee@gmail.com>
2022-04-18 23:23:17 -0400

    Registered the Supers & Super_types App in the Admin.PY file + Modifided the View File for Both App

--- super_types/views.py
+++ super_types/views.py
@@ -1,8 +1,14 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import SuperTypeSerializer
+from .models import SuperType
 
 @api_view(['GET'])
 def super_types_list(request):
 
+  super_type = SuperType.objects.all()
 
-    return Response('ok')
+  serializer = SuperTypeSerializer(super_type, many=True)
+
+
+  return Response(serializer.data)
--- supers/views.py
+++ supers/views.py
@@ -1,12 +1,17 @@
 import imp
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import SupersSerializer
+from .models import Supers
 
 
 @api_view(['GET'])
 def supers_list(request):
+
+  super = Supers.objects.all()
+
+  serializer = SupersSerializer(super, many=True)
   
   
-  
-    return Response('ok')
+  return Response(serializer.data)
   
